7956c8f21651011e8f93d116e09febdabb022a2e
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-05-07 19:00:29 +0800

    reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 import ru.netology.sqr.SQRService;
 
 public class Main {
-public static void main (String[]args){
-    SQRService service = new SQRService();
-    int res = service.squreCount (200, 300);
-    System.out.println(res);
-}
+    public static void main(String[] args) {
+        SQRService service = new SQRService();
+        int res = service.squreCount(200, 300);
+        System.giout.println(res);
+    }
 }
