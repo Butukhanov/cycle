048b41c41a5040e351da48c539ae391dc849a429
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-05-07 17:22:44 +0800

    initial commit

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.sqr;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQRServiceTest {
+    @org.junit.jupiter.api.Test
+    public void test() {
+        SQRService service = new SQRService();
+        int actual = service.squreCount(200, 300);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+}
